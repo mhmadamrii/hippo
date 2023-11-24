7b9ed7b17a84740c6d215fec04fa6ba9164d16b8
mhmadamrii <interceptorghost4@gmail.com>
2023-11-24 23:21:30 +0700

    formatting the code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,23 +5,21 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-
 export function formatPrice(
   price: number | string,
   options: {
-    currency?: 'USD' | 'EUR' | 'GBP' | 'BDT'
-    notation?: Intl.NumberFormatOptions['notation']
-  } = {}
+    currency?: 'USD' | 'EUR' | 'GBP' | 'BDT';
+    notation?: Intl.NumberFormatOptions['notation'];
+  } = {},
 ) {
-  const { currency = 'USD', notation = 'compact' } = options
+  const { currency = 'USD', notation = 'compact' } = options;
 
-  const numericPrice =
-    typeof price === 'string' ? parseFloat(price) : price
+  const numericPrice = typeof price === 'string' ? parseFloat(price) : price;
 
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency,
     notation,
     maximumFractionDigits: 2,
-  }).format(numericPrice)
+  }).format(numericPrice);
 }
